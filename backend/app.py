b95f8059496d7fb5d7ca4fa73d16853190e47121
Yc Wang <swyc168@gmail.com>
2024-10-12 15:35:54 +1100

    UpdateApp - Add new path /reset to reset the counter

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,15 @@ app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
-def hello():
-    redis.incr('hits')
-    counter = str(redis.get('hits'),'utf-8')
-    return f"This webpage has been hit {counter} time(s)\n"
+def incr():
+  redis.incr('hits')
+  counter = str(redis.get('hits'),'utf-8')
+  return f"This webpage has been hit {counter} time(s)\n"
+
+@app.route('/reset')
+def reset():
+  redis.set('hits', 0)
+  return "This webpage view counter has been reset\n"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+  app.run(host="0.0.0.0", port=8000, debug=True)
