bd4988ef3ebad974fcbca61c90112fd0ad79f810
Yc Wang <swyc168@gmail.com>
2024-11-01 15:38:46 +1100

    UpdateInfra - Update iam module args

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,11 +7,26 @@ terraform {
   }
 }
 
+variable "thumbprint" {
+  type = string
+}
+
+variable "audience" {
+  type = string
+}
+
+variable "repo_name" {
+  type = string
+}
+
 provider "aws" {
   region = "ap-southeast-2"
 }
 
 module "oidc" {
-  source = "./modules/oidc"
+  source = "./modules/iam"
+  repo_name = var.repo_name
+  provider_thumbprint = var.thumbprint
+  audience = var.audience
   provider_url = "https://token.actions.githubusercontent.com"
 }
